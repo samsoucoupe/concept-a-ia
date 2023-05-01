ac1bba4c8457639821bf0bff0db4450e84cc7702
samyBendhiab <samybendhiab06@outlook.fr>
2023-05-02 01:29:01 +0200

    add UIX

--- test_converter.sh
+++ test_converter.sh
@@ -1,21 +1,36 @@
-#River False:
-variable="LoupChevreSaladeEtudiant"
-# nom du fichier
-echo "Enter the name of the file (without the extension):"
-#read variable
-echo "Enter the river (True or False):"
-
-river="True"
+utils/Interface/Introduction.sh
 
 
+echo "Did you want to use the rules in the Rules folder (True or False):"
+read use
+if [ $use = "True" ] || [ $use = "true" ]
+then
+  echo "List of the rules in the Rules folder:"
+  ls Rules/
+  echo "Enter the name of the file ( For example: Rules_LCS.json => LCS):"
+  read variable
+else
+  echo "Enter a name for the rules:"
+  read variable
+  python3 utils/question.py --nom_fichier $variable
+fi
+
+echo "The problem is: $variable"
+echo "Enter if the problem is a river crossing problem (True or False):"
+read river
+if [ $river = "True" ] || [ $river = "true" ]
+then
+  echo "The problem is a river crossing problem"
+  river="true"
+else
+  echo "The problem is not a river crossing problem"
+  river="false"
+fi
 
 
-
-python3 utils/converter.py -r $river -type xtd -input XML/$variable.xml -output DOT/test/$variable.dot
+python3 utils/generator.py --rulesfiles Rules/Rules_$variable.json --output XML/test/$variable.xml
+python3 utils/converter.py -r $river -type xtd -input XML/test/$variable.xml -output DOT/test/$variable.dot
 dot -Tpng DOT/test/$variable.dot -o PNG/test/$variable.png
-python3 utils/converter.py -r $river -type dtx -input DOT/test/$variable.dot -output XML/test/$variable.xml
-##java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n 10 -print 0 -resultsType 1 -crossingRiver false -file  XML/$variable.xml
-#java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n 10 -print 0 -resultsType 1 -crossingRiver $river -file  XML/$variable.xml
-#echo ""
-#java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n 10 -print 0 -resultsType 1 -crossingRiver False -file  XML/test/$variable.xml
+java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n 10 -print 0 -resultsType 1 -crossingRiver $river -file  XML/test/$variable.xml
 
+utils/Interface/Conclution.sh
