60502144ba25a5bb9d09693a37c892d8b7cb9b5e
samyBendhiab <samybendhiab06@outlook.fr>
2023-05-19 17:17:29 +0200

    use de genere_solution.sh

--- main.sh
+++ main.sh
@@ -25,61 +25,30 @@ then
   ls Rules/
   echo "Enter the name of the file ( For example: Rules_LCS.json => LCS):"
   read variable
+  echo "How many steps do you want to use for the solution ?"
+  read max
 else
   echo "Enter a name for the rules:"
   read variable
-  python3 utils/question.py nom_fichier $variable
+  python3 utils/question.py -f $variable
   python3 utils/generator.py --rulesfiles Rules/Rules_$variable.json --output XML/$variable.xml
-
+  max=$(cat Rules/Rules_$variable.json | grep "solution_max" | cut -d':' -f2 | cut -d',' -f1 | cut -d' ' -f2)
 fi
 
 echo "The problem is: $variable"
 
 #nombre de coup max pour la solution
-echo "Enter the maximum number of moves for the solution:"
-read max
+
 
 echo "Is it a river problem ? (True or False):"
 read river
 if [ $river = "True" ] || [ $river = "true" ]
 then
   echo "The problem is a river crossing problem"
-  river="true"
+  utils/genere_solution.sh true $variable $max
 else
   echo "The problem is not a river crossing problem"
-  river="false"
+  utils/genere_solution.sh false $variable $max
 fi
-
-
-
-mkdir -p PNG/$variable
-mkdir -p DOT/$variable
-
-
-rm -rf PNG/$variable/*
-rm -rf DOT/$variable/*
-
-
-
-if [ $river = "True" ] || [ $river = "true" ]
-then
-  python3 utils/converter.py -r true -type xtd --input XML/$variable.xml --output DOT/$variable/$variable.dot
-  java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n $max -print 0 -resultsType 1 -crossingRiver true -file  XML/$variable.xml
-  java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n $max -print 0 -resultsType 1 -crossingRiver false -file  XML/$variable.xml > TXT/$variable.txt
-  python3 utils/dot_solution.py $variable true
-else
-  python3 utils/converter.py -r false -type xtd --input XML/$variable.xml --output DOT/$variable/$variable.dot
-  java -cp talosExamples-0.4-SNAPSHOT-jar-with-dependencies.jar StateGraph -n $max -print 0 -resultsType 1 -crossingRiver false -file  XML/$variable.xml > TXT/$variable.txt
-  python3 utils/dot_solution.py $variable false
-fi
-
-
-list=$(ls DOT/$variable)
-
-for i in $list
-do
-  dot -Tpng DOT/$variable/$i -o PNG/$variable/$i.png
-done
-
 
 utils/Interface/Conclution.sh
