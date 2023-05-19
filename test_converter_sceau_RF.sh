fe560c20f0add9d97ad8af0230e2fe6e71425db3
samyBendhiab <samybendhiab06@outlook.fr>
2023-05-19 02:09:18 +0200

    update

--- test_converter_sceau_RF.sh
+++ test_converter_sceau_RF.sh
@@ -1,5 +1,6 @@
+
 ################################################################################
-# Copyright (c) 2023. Samy Ben dhiab All rights reserved.                      #
+# Copyright (c) 2023. Samy Ben Dhiab (Samsoucoupe) All rights reserved.        #
 #                                                                              #
 #    Licensed under the Apache License, Version 2.0 (the "License");           #
 #    you may not use this file except in compliance with the License.          #
