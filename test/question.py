7f61b034aa2bf4ab443bf8eb405381b39d7356e7
samyBendhiab <samybendhiab06@outlook.fr>
2023-05-19 02:12:02 +0200

    update copyrigth

--- test/question.py
+++ test/question.py
@@ -1,7 +1,33 @@
+#  Copyright (c) 2023. Samy Ben Dhiab (Samsoucoupe) All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#   #
+#         http://www.apache.org/licenses/LICENSE-2.0
+#   #
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#   #
+#         http://www.apache.org/licenses/LICENSE-2.0
+#   #
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import argparse
 import json
 import os
-import sys
-import argparse
+
 
 def init_element_list(type_de_probleme, nb_element, boat, boat_size):
     actions = {"vider": 0, "remplir": 0, "transferer": 0}
@@ -15,6 +41,7 @@ def init_element_list(type_de_probleme, nb_element, boat, boat_size):
                     "regle_obligatoire": [],
                     "regle_traverser": None}
     return {"element": element, "info_problem": info_problem}
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="name of the file to create")
@@ -46,7 +73,6 @@ else:
 
 boat_size = 1
 
-
 print(separateur)
 
 nb_element = input("Nombre de sceau: " if type_de_probleme == "sceau" else "Nombre d'element distinct: ")
@@ -60,7 +86,7 @@ print(separateur)
 nb_version = 1
 for i in range(nb_element):
     if type_de_probleme == "sceau":
-        print(f"Sceau {i+1} - ")
+        print(f"Sceau {i + 1} - ")
         taille_sceau = input("Quantité max du sceau en L: ")
         while not taille_sceau.isdigit():
             taille_sceau = input("Quantité max du sceau en L: ")
@@ -75,7 +101,7 @@ for i in range(nb_element):
         liste_element["element"][i] = {"name": name, "quantite": quantite, "incompatible": ""}
 
     elif type_de_probleme == "river":
-        print(f"Element {i+1}")
+        print(f"Element {i + 1}")
         name = input("nom : ")
         liste_name_use = [liste_element["element"][elt]["name"] for elt in liste_element["element"]]
         while name in liste_name_use:
@@ -91,7 +117,7 @@ for i in range(nb_element):
         poids = input("Combien peuvent etre transporter par le bateau ? ")
         while not poids.isdigit():
             poids = input("Combien peuvent etre transporter par le bateau ? ")
-        poids = 1/int(poids)
+        poids = 1 / int(poids)
         liste_element["element"][i] = {"name": name, "quantite": quantite, "incompatible": "", "poids": poids}
 
 print(separateur)
@@ -104,8 +130,10 @@ for i in range(nb_element):
     liste_element['info_problem']['position_final'].append(position_final)
 
 if type_de_probleme == "river":
-    liste_element['info_problem']['position_initial']=[int(elt) for elt in liste_element['info_problem']['position_initial']]+[1]
-    liste_element['info_problem']['position_final']=[int(elt) for elt in liste_element['info_problem']['position_final']]+[0]
+    liste_element['info_problem']['position_initial'] = [int(elt) for elt in
+                                                         liste_element['info_problem']['position_initial']] + [1]
+    liste_element['info_problem']['position_final'] = [int(elt) for elt in
+                                                       liste_element['info_problem']['position_final']] + [0]
 print(separateur)
 
 B_regles = input("Y a t'il des regles a ajouter ? (oui ou non) ")
@@ -138,10 +166,9 @@ if B_regles and type_de_probleme == "sceau":
     print(separateur)
 
 else:
-    liste_element["info_problem"]["actions_possible"]= {"vider": 1, "remplir": 1, "transferer": 1}
+    liste_element["info_problem"]["actions_possible"] = {"vider": 1, "remplir": 1, "transferer": 1}
 
-
-if B_regles and  type_de_probleme == "river":
+if B_regles and type_de_probleme == "river":
     print("Les participants doivent-ils être en nombre égal, supérieur ou aucun des deux ?")
     regles = input("0 : égal, 1 : supérieur, 2: supérieur ou égal, 3 : aucun ")
     while regles not in ["0", "1", "2", "3"]:
@@ -155,17 +182,16 @@ if B_regles and  type_de_probleme == "river":
         while not id_sup.isdigit() or int(id_sup) > nb_element:
             id_sup = input("Quel est l'ID de l'élément supérieur ? (Exemple: 1: LOUP 2: CHEVRE 3: SALADE => 1) ")
 
-
     if regles == "0" or regles == "2":
         liste_element["info_problem"]["regle_obligatoire"].append("EGALE")
     if regles == "1" or regles == "2":
         liste_element["info_problem"]["regle_obligatoire"].append("SUPERIEUR")
         liste_element["info_problem"]["regle_obligatoire"].append(id_sup)
 
-
     print(separateur)
 
-    print("Voulez-vous limiter le nombre de chaque élément qui se déplace sur le bateau ? (Exemple : 1 de chaque élément maximum) ")
+    print(
+        "Voulez-vous limiter le nombre de chaque élément qui se déplace sur le bateau ? (Exemple : 1 de chaque élément maximum) ")
     regles = input("0 : oui, 1 : non ")
     while regles not in ["0", "1"]:
         regles = input("0 : oui, 1 : non ")
@@ -180,8 +206,6 @@ if B_regles and  type_de_probleme == "river":
 else:
     liste_element["info_problem"]["regle_obligatoire"] = []
     liste_element["info_problem"]["regle_traverser"] = ""
-
-
 
 # Ecriture du fichier json
 with open(f"{pwd}/{nom_fichier}.json", "w") as f:
