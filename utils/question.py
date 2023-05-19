d6301b3c5581f8c1557c40630ab8e64fcb07aa4f
samyBendhiab <samybendhiab06@outlook.fr>
2023-05-19 17:17:12 +0200

    ajout de solution max

--- utils/question.py
+++ utils/question.py
@@ -1,28 +1,5 @@
+
 #  Copyright (c) 2023. Samy Ben Dhiab (Samsoucoupe) All rights reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#   #
-#         http://www.apache.org/licenses/LICENSE-2.0
-#   #
-#     Unless required by applicable law or agreed to in writing, software
-#     distributed under the License is distributed on an "AS IS" BASIS,
-#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#     limitations under the License.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#   #
-#         http://www.apache.org/licenses/LICENSE-2.0
-#   #
-#     Unless required by applicable law or agreed to in writing, software
-#     distributed under the License is distributed on an "AS IS" BASIS,
-#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#     limitations under the License.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -50,8 +27,10 @@ def init_element_list(type_de_probleme, nb_element, boat, boat_size):
                     "position_final": [],
                     "boat_only": boat,
                     "boat_size": boat_size,
+                    "solution_max": 10,
                     "regle_obligatoire": [],
-                    "regle_traverser": None}
+                    "regle_traverser": ""
+                    }
     return {"element": element, "info_problem": info_problem}
 
 
@@ -216,6 +195,11 @@ if not B_regles:
     liste_element["info_problem"]["regle_obligatoire"] = []
     liste_element["info_problem"]["regle_traverser"] = ""
     liste_element["info_problem"]["actions_possible"] = {"vider": 1, "remplir": 1, "transferer": 1}
+
+max_solution = input("Combien de coups maximum pour la solution ? ")
+while not max_solution.isdigit():
+    max_solution = input("Combien de coups maximum pour la solution ? ")
+liste_element["info_problem"]["max_solution"] = int(max_solution)
 
 # Ecriture du fichier json
 with open(f"{pwd}/{nom_fichier}.json", "w") as f:
